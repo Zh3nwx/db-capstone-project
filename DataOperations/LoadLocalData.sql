e6d9aa32db2fccd416267d78a5b67419a5451a10
Zh3nwx <137427645+Zh3nwx@users.noreply.github.com>
2024-01-12 10:45:33 -0800

    Update LoadLocalData.sql

--- DataOperations/LoadLocalData.sql
+++ DataOperations/LoadLocalData.sql
@@ -1,3 +1,4 @@
+#You must also edit the connection of your MySQL Connection, navigate to the advance tab and add in " OPT_LOCAL_INFILE=1"
+
 SET GLOBAL local_infile=1;
 SHOW GLOBAL VARIABLES LIKE 'local_infile';
-
