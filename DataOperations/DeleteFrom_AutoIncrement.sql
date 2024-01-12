00416a41b1b4c933ff7519afde7ed015f6d79a69
Zh3nwx <137427645+Zh3nwx@users.noreply.github.com>
2024-01-12 11:01:09 -0800

    Update DeleteFrom_AutoIncrement.sql

--- DataOperations/DeleteFrom_AutoIncrement.sql
+++ DataOperations/DeleteFrom_AutoIncrement.sql
@@ -1,4 +1,5 @@
-## Delete data from indicated table, sets auto increment to 1
+# Delete data from indicated table, sets auto increment to 1
+#There was a few scenarios where I had to reset the auto-increment count to 1. This is the way to do it. Replace the "table name" after DELETE FROM.
 
 DELETE FROM Delivery;
 
